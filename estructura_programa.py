209fbe32700dcb3327d004c029c2c811994711a1
ramossinflores <mlaura.ragra@gmail.com>
2024-07-22 00:17:24 +0200

    ADD: Clase sobre estructura básica de un programa y apuntes en Jupyter Notebook
    Los apuntes están en desarrollo

---
+++ estructura_programa.py
@@ -0,0 +1,30 @@
+# Importar módulos
+import math # Al inicio del código para que funcione en todo el código
+print(math.sqrt(25))
+#Declaración de variables 
+A = "Juan"
+E = 30
+
+#Funciones 
+def saludar(nombre):
+    print ("Hola" + nombre +"!")
+
+# if, else, elseif
+if edad >= 8:
+    print("Eres mayor de edad")
+else:
+    print("Eres menorde edad")
+
+# for, while
+edad=10
+
+for i in range(5):
+    print(i)
+
+while edad < 30:
+    print("Eres joven")
+    edad +=1
+
+# Dar entrada / salida a una variable
+nombre = input("Ingrese su nombre: ")
+print("Hola, " + nombre + "!") 
